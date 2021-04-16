9572d5b7756745c36298b422afa32473ba764f03
cedric <cc>
2021-04-16 20:11:44 +0200

    ggg

--- Accueil.php
+++ Accueil.php
@@ -2,7 +2,7 @@
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
-and open the template in the editor.
+and open the template in the editor.ddd
 -->
 <html>
     <head>
