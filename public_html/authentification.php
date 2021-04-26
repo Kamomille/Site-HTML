afe1a50eb2485bf01e846549bda4e30942c0a637
cedric <cc>
2021-04-26 11:32:40 +0200

    test

--- public_html/authentification.php
+++ public_html/authentification.php
@@ -13,25 +13,25 @@
     }
     
     */
-
-    $res = $database-> query("SELECT * FROM authentification where (identifiant='$identifiant' AND mdp='$mdp');");
-    $res =$res->fetchall();
-    $count=sizeof($res); 
+    $res = mysqli_query($connect,"SELECT * FROM authentification where (identifiant='$identifiant' AND mdp='$mdp');");
+    $res = mysqli_fetch_all($res);
+    print_r($res);
+    $count=sizeof($res);
     
     if ($count!=0) {
         session_start();
-        $_SESSION['id']=$res[0]['id'];
-        $_SESSION['identifiant']=$res[0]['identifiant']; 
-        $_SESSION['mdp']=$res[0]['mdp'] ;
-        $_SESSION['nom']=$res[0]['nom'] ;
-        $_SESSION['prenom']=$res[0]['prenom']; 
-        $_SESSION['mail']=$res[0]['mail'] ;
-        $_SESSION['tel']=$res[0]['tel'] ;
-        $_SESSION['contrat']=$res[0]['contrat']; 
-        $_SESSION['situationFamiliale']=$res[0]['situationFamillial'] ;
-        $_SESSION['adresse']=$res[0]['adresse'] ;
-        $_SESSION['age']=$res[0]['age'] ;
-        echo $_SESSION['situationFamiliale'];
+        $_SESSION['id']=$res[0][0];
+        $_SESSION['identifiant']=$res[0][1]; 
+        $_SESSION['mdp']=$res[0][2] ;
+        $_SESSION['nom']=$res[0][3] ;
+        $_SESSION['prenom']=$res[0][4]; 
+        $_SESSION['mail']=$res[0][5] ;
+        $_SESSION['tel']=$res[0][6] ;
+        $_SESSION['contrat']=$res[0][7]; 
+        $_SESSION['situationFamiliale']=$res[0][8] ;
+        $_SESSION['adresse']=$res[0][9] ;
+        $_SESSION['age']=$res[0][10] ;
+        $_SESSION['fonction']=$res[0][11];
         header("Location:http://localhost/projetSite_HTML/public_html/menu.html");
         
         
