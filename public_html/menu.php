fb4d01722cee485726ee3a66fd8e9a4962dfd534
Kamomille <camille.bayon-de-noyer@esme.fr>
2021-04-26 16:51:26 +0200

    test

---
+++ public_html/menu.php
@@ -0,0 +1,24 @@
+<?php
+
+session_start() ;
+
+
+echo "<html>";
+
+echo "<table>";
+
+echo "<tr>";
+    echo "<td><a href=gestionProfil.php>Gestion Profil</a></td>";
+    echo "<td><a href='contacts.php'>Contacts</a></td>";
+    echo "<td><a href='gestionSalarié.php'>Gestion Salariés</a></td>";
+    if (strcmp($_SESSION['role'], 'Salarié') == 0) {
+        echo "<td><a href='gestionConges_salaries.html'>Gestion Congés</a></td>";
+    }
+    if (strcmp($_SESSION['role'], 'Directeur') == 0) {
+        echo "<td><a href='gestionConges_directeur.html'>Gestion Congés</a></td>";
+    }
+echo "</tr>";
+
+echo "</table><html>";
+
+?>
