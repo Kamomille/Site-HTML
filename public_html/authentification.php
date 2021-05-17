67fa3df871dd531e7762dcffb8b4173d622f77cb
Kamomille <camille.bayon-de-noyer@esme.fr>
2021-05-17 12:08:46 +0200

    authentification vérifcation

--- public_html/authentification.php
+++ public_html/authentification.php
@@ -10,61 +10,78 @@
         $saisie_mdp=$_POST['mot_de_passe'];
         $saisie_identifiant=$_POST['login'];
         $saisie_idRadio=$_POST['idRadio'];
+        
+        $autentif = authentification($saisie_mdp,$saisie_identifiant);
+        
+        if (strcmp($autentif,'mdp FAUX') == 0) :
+            echo "<script>alert(\"Votre mot de passe est incorrect\")</script>";
 
-        if (strcmp(authentification($saisie_mdp,$saisie_identifiant), 'FAUX') != 0) {
+        elseif (strcmp($autentif, 'login FAUX') == 0) :
+            echo "<script>alert(\"Aucun compte associé à ce login.\")</script>";
+
+        elseif (strcmp($autentif, 'statut FAUX') == 0) :
+            echo "<script>alert(\"Statut faux.\")</script>";
+
+        else:
+
             setcookie("identifiant",$_SESSION['identifiant'],time()+3600*24*2);
             setcookie("role",$_SESSION['role'],time()+3600*24*2);
-            setcookie("id",$_SESSION['id'],time()+3600*24*2);
+            setcookie("id",$_SESSION['id'],time()+3600*24*2);            var_dump($_SESSION);
             header("Location:http://localhost/projetSite_HTML/public_html/menu.php");  
-        }
-        else{
-            echo "<script>alert(\"Incorrect\")</script>";
-        }        
+        endif;
+             
     }
 
     
 function authentification($saisie_mdp,$saisie_identifiant){
     include 'database.php';
     if($connect) {
-        $req = 'SELECT id,identifiant, mdp, fonction FROM authentification WHERE (identifiant=? AND mdp=?);';
-        $result = mysqli_prepare($connect,$req);
-        $var= mysqli_stmt_bind_param($result,'ss',$saisie_identifiant,$saisie_mdp);
-        $var=mysqli_execute($result);
-        mysqli_stmt_bind_result($result,$id,$identifiant,$mdp,$fonction);
-        $return = 'FAUX';
+        $req = 'SELECT id,identifiant, mdp, fonction FROM authentification;';
+        $resultat = mysqli_prepare($connect,$req);
+        $var=mysqli_execute($resultat);
+        mysqli_stmt_bind_result($resultat,$id,$identifiant,$mdp,$fonction);
         
-        while (mysqli_stmt_fetch($result)){
-            if ($saisie_mdp==$mdp && $saisie_identifiant==$identifiant){
-                if (strcmp($_POST['idRadio'], 'Directeur') == 0 && strcmp($fonction, "directeur")==0){
-                    $return='Directeur';
-                    setcookie("mdp",$mdp,time()+3600*24*2);
-                    break;
+        if($resultat == false) echo "Echec de l'exécution de la requête";
+        else {
+            $return = 'FAUX';
+            while ( mysqli_stmt_fetch($resultat)){
+                if (strcmp($saisie_identifiant,$identifiant)!=0 && strcmp($return,'statut FAUX') != 0 && strcmp($return,'mdp FAUX') != 0){
+                        $return = 'login FAUX';
                 }
-                if (strcmp($_POST['idRadio'], 'Salarié') == 0 && strcmp($fonction, "enseignant")==0){
-                    $return='Salarié';
-                    setcookie("mdp",$mdp,time()+3600*24*2);
-                    break;
+                if ($saisie_identifiant==$identifiant && $saisie_mdp!=$mdp && strcmp($return,'statut FAUX') != 0){
+                    $return = 'mdp FAUX';
                 }
-                if (strcmp($_POST['idRadio'], 'Salarié') == 0 && strcmp($fonction,"administration")==0){
-                    $return='Salarié';
-                    setcookie("mdp",$mdp,time()+3600*24*2);
-                    break;
+                if ($saisie_mdp==$mdp && $saisie_identifiant==$identifiant){
+                    $return = 'statut FAUX';
+                    if (strcmp($_POST['idRadio'], 'Directeur') == 0 && strcmp($fonction, "directeur")==0){
+                        $return='Directeur';
+                        setcookie("mdp",$mdp,time()+3600*24*2);
+                        break;
+                    }
+                    if (strcmp($_POST['idRadio'], 'Salarié') == 0 && strcmp($fonction, "enseignant")==0){
+                        $return='Salarié';
+                        setcookie("mdp",$mdp,time()+3600*24*2);
+                        break;
+                    }
+                    if (strcmp($_POST['idRadio'], 'Salarié') == 0 && strcmp($fonction,"administration")==0){
+                        $return='Salarié';
+                        setcookie("mdp",$mdp,time()+3600*24*2);
+                        break;
+                    }
                 }
             }
         }
-        if($return!='FAUX'){
-            session_start();
-            $_SESSION['id']=$id;
-            $_SESSION['identifiant']=$identifiant;
-            $_SESSION['mdp']=$mdp;
-            $_SESSION['role']=$return;
-            $_SESSION['fonction']=$fonction;
-            mysqli_stmt_close($result);
-            mysqli_close($connect);            
-        }
-
     }
-
+    if($return!='FAUX'){
+        session_start();
+        $_SESSION['id']=$id;
+        $_SESSION['identifiant']=$identifiant;
+        $_SESSION['mdp']=$mdp;
+        $_SESSION['role']=$return;
+        $_SESSION['fonction']=$fonction;
+        mysqli_stmt_close($resultat);
+        mysqli_close($connect);            
+    }
     return $return;
 }
 
@@ -107,12 +124,10 @@ function authentification($saisie_mdp,$saisie_identifiant){
                 </tr>
                     <td><label for="mdp">Mot de passe :</label></td>
                     <td><input type="password" name="mot_de_passe" required/></td>
-                   
                 </tr>      
                 <tr>
                     <td><input type="submit" value="Valider" name="submit" /></td>
                 </tr>    
-                
                 </table>
             </fieldset>
     </body>
