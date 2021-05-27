b148d2ebe1e6d0f8f48e342f23f005dd04edcad5
Kamomille <camille.bayon-de-noyer@esme.fr>
2021-05-27 21:07:10 +0200

    menu

--- public_html/menu.php
+++ public_html/menu.php
@@ -1,28 +1,7 @@
-<?php
-
-session_start() ;
-if(isset($_COOKIE)){
-    $_SESSION['id']=$_COOKIE['id'];
-    $_SESSION['identifiant']=$_COOKIE['identifiant'];
-    $_SESSION['mdp']=$_COOKIE['mdp'];
-    $_SESSION['fonction']=$_COOKIE['fonction'];
-    if($_SESSION['fonction']=='directeur'){
-        $_SESSION['role']='directeur';
-    }
-    else {
-        $_SESSION['role']='salarie';
-    }
-}
- else {
-     header("Location:http://localhost/projetSite_HTML/public_html/index.html"); 
-}
-
-?>
-
 <html>
     
     <head>
-        <title>Esme gestion congé</title>
+        <title>Menu</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="page.css" rel="stylesheet" type="text/css">
@@ -31,31 +10,20 @@ if(isset($_COOKIE)){
     <body>
         <header>
             <img src="image\Esme_logo.png" class='esme'>     
-            <h1 class="accueil">Gestion des congés</h1>  
+            <h1 class="accueil">Menu</h1>  
             <img src="image\devise.jpg" class="devise">
         </header>
 
 
-    <?php include("haut_page.php");
+    <?php 
+    include 'database.php';
+    include("haut_page.php");
 
-    if (strcmp($_SESSION['fonction'], 'enseignant') == 0 || strcmp($_SESSION['fonction'], 'administration') == 0){
-        echo "<a class='nav' href='consultationCommentaire.php'>Commentaire</a>";
+    if(isset($_COOKIE)){
+        $prenom=$_COOKIE['prenom'];
+        $nom=$_COOKIE['nom'];
     }
-    else {
-        echo "<a class='nav' href='consultationCommentaire.php'>Commentaires</a>";
-    }
-
-    if (strcmp($_SESSION['fonction'], 'enseignant') == 0 || strcmp($_SESSION['fonction'], 'administration') == 0) {
-        echo "<a class='nav' href='gestionConges_salaries.php'>Gestion Congés</a>";
-    }
-    if (strcmp($_SESSION['fonction'], 'directeur') == 0) {
-        echo "<a class='nav' href='gestionConges_directeur.php'>Gestion Congés</a>";
-    }
-    echo "<a class='nav' href='ajout_CV.php'>test ajout CV</a>";
-
-                ?>
-            </nav>
-
-    <?php include("pied_de_page.php"); ?>
+    echo '<h1>'.'Bienvenue '.$prenom.' '.$nom.'</h1>';
+    include("pied_de_page.php"); ?>
     </body>
 </html>
